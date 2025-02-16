b75a9c2a722fff79a9c84f38bbdd2935141a8c71
suryachithra <suryachithra28@gmail.com>
2025-02-16 18:38:19 +0530

    Assignment 8 - Employee Name and Hike percentage

--- src/com/assignments/Assignment8.java
+++ src/com/assignments/Assignment8.java
@@ -1,63 +1,72 @@
 package com.assignments;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Assignment8 {
 
 	public static void main(String[] args) {
-		
-		Map<String, String> aliceDetails = new LinkedHashMap <String,String>();
-		aliceDetails.put("Name", "Alice Johnson");
-		aliceDetails.put("Base Salary", "75000");
-		aliceDetails.put("Experience (Years)", "5");
-		aliceDetails.put("Year-End Rating(out of 5)", "4.2");
-		
-		Map<String, String> bobDetails = new LinkedHashMap <String,String>();
-		bobDetails.put("Name", "Bob Smith");
-		bobDetails.put("Base Salary", "68000");
-		bobDetails.put("Experience (Years)", "3");
-		bobDetails.put("Year-End Rating(out of 5)", "3.8");
-		
-		Map<String, String> carolDetails = new LinkedHashMap <String,String>();
-		carolDetails.put("Name", "Carol Davis");
-		carolDetails.put("Base Salary", "82000");
-		carolDetails.put("Experience (Years)", "7");
-		carolDetails.put("Year-End Rating(out of 5)", "4.5");
-		
-		Map<String, String> davidDetails = new LinkedHashMap <String,String>();
-		davidDetails.put("Name", "David Brown");
-		davidDetails.put("Base Salary", "90000");
-		davidDetails.put("Experience (Years)", "10");
-		davidDetails.put("Year-End Rating(out of 5)", "2");
-		
-		Map<String, String> evaDetails = new LinkedHashMap <String,String>();
-		evaDetails.put("Name", "Eva Green");
-		evaDetails.put("Base Salary", "60000");
-		evaDetails.put("Experience (Years)", "2");
-		evaDetails.put("Year-End Rating(out of 5)", "3.5");
-		
-		
-		  List<Map<String, String>> employeeDetails = new ArrayList<Map<String, String>>(); 
-		  employeeDetails.add(aliceDetails);
-		  employeeDetails.add(bobDetails);
-		  employeeDetails.add(carolDetails); 
-		  employeeDetails.add(davidDetails);
-		  employeeDetails.add(evaDetails);
-		  
-		  System.out.println(employeeDetails.get(0).get("Name"));
-		  System.out.println(employeeDetails.size());
-		 
-		
-		System.out.println(aliceDetails.get("Base Salary"));
-		
-		String[] rating = {aliceDetails.get("Year-End Rating(out of 5)"),bobDetails.get("Year-End Rating(out of 5)"),carolDetails.get("Year-End Rating(out of 5)"),davidDetails.get("Year-End Rating(out of 5)"),evaDetails.get("Year-End Rating(out of 5)")};
-		
-		
-		 
-		
-	}
 
+		String[] employeeName = { "Alice Johnson", "Bob Smith", "Carol Davis", "David Brown", "Eva Green" };
+
+		Double[] aliceDetails = { 75000.00, 5.0, 4.2 };
+		Double[] bobDetails = { 68000.00, 3.0, 3.8 };
+		Double[] carolDetails = { 82000.00, 7.0, 4.5 };
+		Double[] davidDetails = { 90000.00, 10.0, 2.0 };
+		Double[] evaDetails = { 60000.00, 2.0, 3.5 };
+
+		Map<String, Double[]> employeeDetails = new LinkedHashMap<String, Double[]>();
+		employeeDetails.put(employeeName[0], aliceDetails);
+		employeeDetails.put(employeeName[1], bobDetails);
+		employeeDetails.put(employeeName[2], carolDetails);
+		employeeDetails.put(employeeName[3], davidDetails);
+		employeeDetails.put(employeeName[4], evaDetails);
+
+		// Map to store hike percentage for each employee
+		Map<String, Double> hikePercentageAndNamemap = new HashMap<>();
+
+		// Calculate hike for each employee
+		for (int i = 0; i < employeeName.length; i++) {
+
+			String employee = employeeName[i];
+
+			Double[] employeeData = employeeDetails.get(employee);
+
+			double baseSalary = employeeData[0];
+			double experience = employeeData[1];
+			double rating = employeeData[2];
+
+			double variablePayPercent;
+			double bonus;
+			double reward = 0;
+
+			if (rating >= 4) {
+				variablePayPercent = 15;
+				bonus = 1500;
+			} else if (rating >= 3) {
+				variablePayPercent = 10;
+				bonus = 1200;
+			} else {
+				variablePayPercent = 3;
+				bonus = 300;
+			}
+
+			if (experience >= 5) {
+				reward = 5000;
+			}
+
+			// Calculating hike percentage
+			double hike = (baseSalary * variablePayPercent/100) + bonus + reward;
+			double hikePercentage = (hike / baseSalary)*100;
+
+			// Storing name of each employee with their hike percentage
+			hikePercentageAndNamemap.put(employee, hikePercentage);
+		}
+		
+		// Printing Employee name and Hike percentage
+		System.out.println("Hike percentage of each employee");
+		System.out.println(hikePercentageAndNamemap);
+
+	}
 }
