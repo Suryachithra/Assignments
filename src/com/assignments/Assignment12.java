eeab4cb11efdb94ee9a07e1da13bc3c8a8a52dbb
suryachithra <suryachithra28@gmail.com>
2025-02-17 13:13:55 +0530

    Assignment 12 - Prime number

---
+++ src/com/assignments/Assignment12.java
@@ -0,0 +1,29 @@
+package com.assignments;
+
+public class Assignment12 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Calling the method and printing "true" for prime number //and "false" for
+		 * non prime number;
+		 */		
+		System.out.println(primeNumber(9));
+	}
+
+	//Method to find whether a number is prime or not
+	public static boolean primeNumber(int n) {
+
+		if (n <= 1) {
+			return false;
+		}
+
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false; // n is divisible by i, so it's not prime
+			}
+		}
+		return true;
+
+	}
+}
