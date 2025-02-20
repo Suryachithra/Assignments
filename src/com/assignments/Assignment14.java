cf758a2cbd4a52f95ce23e518492b1dd3a309310
suryachithra <suryachithra28@gmail.com>
2025-02-20 16:09:30 +0530

    Assignment 14 - String operations

--- src/com/assignments/Assignment14.java
+++ src/com/assignments/Assignment14.java
@@ -9,23 +9,29 @@ public class Assignment14 {
 		String[] words = sentence.split(" ");
 		int wordCount = words.length;
 		System.out.println("Total number of words in the given sentence is " + wordCount);
-		
-		//Convert the first character of each word to uppercase and print original sentence
-		for(int i =0; i<words.length; i++) {
-			char firstLetter = words[i].charAt(0);
-			String letter = String.valueOf(firstLetter);
-			System.out.println(letter.toUpperCase());
-			
-			
-		}
 
 		// Print the sentence words in reverse order.
-		for (int i = sentence.length() - 1; i >= 0; i--) {
-			System.out.print(sentence.charAt(i));
+		for (int i = wordCount - 1; i >= 0; i--) {
+			System.out.print(words[i] + " ");
 		}
-		
+		System.out.println();
 		
 
+		/*
+		 * Convert the first character of each word to uppercase and print original
+		 * sentence
+		 */
+		StringBuilder result = new StringBuilder();
+		//For loop to convert first character of each word to uppercase.
+        for (String word : words) {
+            if (word.length() > 0) {
+                result.append(word.substring(0, 1).toUpperCase());
+                result.append(word.substring(1));  
+                result.append(" ");  
+            }
+        }
+        //Print the modified sentence
+        System.out.println(result);
 	}
 
 }
